c1faff0b459c052da517454e780552aa6385c060
Chae Lin Shin <cls561@nyu.edu>
2025-02-23 15:51:40 +0900

    fix. Only show parenthesis with partOfSpeech when it exists

--- components/WordCard.tsx
+++ components/WordCard.tsx
@@ -10,7 +10,9 @@ export default function WordCard({ word, showDefinition }: WordCardProps) {
         <div className="flex flex-col items-center justify-center bg-gray-100 p-3 h-72 rounded-2xl text-2xl mb-5 shadow-xl">
             <span className="mb-3">{word?.word}</span>
             {showDefinition && word?.definitions.map((def: Definition) => (
-                <div key={def.definition} className="text-lg">({def.partOfSpeech}) {def.definition}</div>
+                <div key={def.definition} className="text-lg">
+                    {def?.partOfSpeech && def.partOfSpeech} {def.definition}
+                </div>
             ))}
         </div>
     );
