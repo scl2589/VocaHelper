d5d8dbf6f92416bea1c7c95723a04d445b83948e
Chae Lin Shin <cls561@nyu.edu>
2025-03-03 23:49:15 +0900

    feat. Add change of color when the definition is shown

--- components/WordCard.tsx
+++ components/WordCard.tsx
@@ -8,7 +8,8 @@ interface WordCardProps {
 export default function WordCard({ word, showDefinition }: WordCardProps) {
     return (
         <div
-            className="flex flex-col items-center pt-28 bg-gray-100 p-3 h-72 rounded-2xl text-2xl mb-5 shadow-xl dark:text-darkWhiteBgText">
+            className={`flex flex-col items-center pt-28 p-3 h-72 rounded-2xl text-2xl mb-5 shadow-xl 
+                ${showDefinition ? "bg-gray-800 text-gray-300" : "bg-gray-300 dark:text-darkWhiteBgText"}`}>
             <span className="mb-3">{word.word}</span>
             {showDefinition && word.definitions.map((def: Definition) => (
                 <div key={def.definition} className="text-lg">
