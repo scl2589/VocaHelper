128329b623d60b8d0d23440ee690038a83c88648
Chae Lin Shin <cls561@nyu.edu>
2025-03-03 23:44:26 +0900

    fix. fix the position of the word

--- components/WordCard.tsx
+++ components/WordCard.tsx
@@ -8,7 +8,7 @@ interface WordCardProps {
 export default function WordCard({ word, showDefinition }: WordCardProps) {
     return (
         <div
-            className="flex flex-col items-center justify-center bg-gray-100 p-3 h-72 rounded-2xl text-2xl mb-5 shadow-xl dark:text-darkWhiteBgText">
+            className="flex flex-col items-center pt-28 bg-gray-100 p-3 h-72 rounded-2xl text-2xl mb-5 shadow-xl dark:text-darkWhiteBgText">
             <span className="mb-3">{word.word}</span>
             {showDefinition && word.definitions.map((def: Definition) => (
                 <div key={def.definition} className="text-lg">
